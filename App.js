e8d06bf64f295184374c239ba59b16af6491301a
Shivam Pandey <shpandey9082@gmail.com>
2024-07-11 17:45:01 +0530

    final copy

--- App.js
+++ App.js
@@ -16,26 +16,58 @@
     - consistent hashing algorithm
     - Zero Configuration
     - Transitive Dependencies
+    - Tree Shaking - Removing un-wanted code.
 
 */
 
 
-import React  from "react";
+//import React  from "react";
+import { createElement as ce } from "react";
 import ReactDOM from "react-dom"
-const heading1 = React.createElement("h1",{
+
+// createElement -> Object-> HTMLcode(DOM)
+
+// react element..further we are never going to use createElement..so elminating this part of code..
+/* const heading1 = ce("h1",{
     id: "header1",
+    key: "h1",
+},"Header1"); */
 
-},"Header1");
+//above is react code which is quite lengthy to type..hence use jsx.
 
-const heading2 = React.createElement("h1",{
-    id:"header2",
-},
-"Header2")
 
-const container = React.createElement("div",{
-    id:"container",
-},
-[heading1,heading2]);
+//jsx for above code :
+//--jsx is html like code in javascript.. not exact html.
+
+//JSX => React.createElement => Object => HTML(DOM)
+//react element..
+const heading = <h1 id = "header1" key="k1">Header1</h1>
+
+/*
+- React Component: 
+    - Functional Component..
+*/
+
+//if it is a component then write {<header />}, also in {} we can write any piece of javascript code.
+
+//component composition or composing components.
+const HeaderComp = ()=> {
+    return (<div>
+        {heading} 
+        <h1 id = "title">Namste React Functional Component.</h1>
+        <h2 id="subtitle">Hii there.</h2>
+    </div>);
+} 
+
+/* 
+For a single expression, you can use implicit return:
+const HeaderComp = () => (
+    <div>
+        {heading}
+        <h1 id="title">Namaste React Functional Component.</h1>
+        <h2 id="subtitle">Hi there.</h2>
+    </div>
+); */
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(container);
+root.render(<HeaderComp />); // way to render react functional component.
